90c109b831dc85414b9c284a95583ad5ceac811f
Gabriela <gabriela.brancalhao@solarprime.com.br>
2022-08-21 13:27:19 -0300

    api

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+  public produtos = [];
+
+  public list_produtos_url = 'http://aai-frontend-interview-mock-data.s3-website-sa-east-1.amazonaws.com/cardlist.json';
+
+  constructor(public http : HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  listarTodosProdutos() {
+    this.http.get(`${ this.list_produtos_url }`)
+             .subscribe(resultado => console.log(resultado));
   }
 
 }
