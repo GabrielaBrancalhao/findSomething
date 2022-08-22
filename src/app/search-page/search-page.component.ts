a89b060dfee3870f00953cf61e7919592714691f
Gabriela <gabriela.brancalhao@solarprime.com.br>
2022-08-22 11:48:24 -0300

    ajustes finos

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 
-  public produtos:any = [];
+  produtos:any = [];
+  todosProdutos: any = [];
 
-  public list_produtos_url = 'http://aai-frontend-interview-mock-data.s3-website-sa-east-1.amazonaws.com/cardlist.json';
+  private list_produtos_url = 'http://aai-frontend-interview-mock-data.s3-website-sa-east-1.amazonaws.com/cardlist.json';
+
+  listProd = [ 
+    {id: 'Type 1', title: 'Paisagem'},
+    {id: 'Type 2', title: 'Flor'},
+    {id: 'Type 3', title: 'Pizza'}
+  ]
 
   constructor(public http : HttpClient) { }
 
@@ -20,9 +27,20 @@ export class SearchPageComponent implements OnInit {
 
   getProdutos() {
     this.http.get(this.list_produtos_url).subscribe((produtos:any)=>{
-      this.produtos = produtos;
-      console.log(produtos)
+      this.todosProdutos = produtos
     })
+    
+  }
+  
+
+  updateFilter(event: any) {
+    this.produtos = this.todosProdutos.filter((p: { produtos: any; }) => p.produtos === val)
+    let val = event.target.value.toLowerCase();
+    this.produtos = this.listProd;
+  }
+
+  excluirCard() { 
+
   }
 
 }
